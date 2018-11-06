6f0fc66c86045dec84ad6923e717bcedbfc6a842
Taizoon Miyajiwala <taizoon.tts@gmail.com>
2018-11-06 00:12:59 -0700

    Solution for "Count number of substrings with numeric value greater than X"

---
+++ CountNumberOfSubStringsGreaterThanX/SubStringsGreaterThanXCounter.cs
@@ -0,0 +1,23 @@
+﻿namespace CountNumberOfSubStringsGreaterThanX
+{
+    public class SubStringsGreaterThanXCounter
+    {
+        public int Count(string str, int x)
+        {
+            var numberOfSubStringsGreaterThanX = 0;
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                for (var j = 1; j <= str.Length - i; j++)
+                {
+                    if (str[i] != '0' && int.Parse(str.Substring(i, j)) > x)
+                    {
+                        numberOfSubStringsGreaterThanX++;
+                    }
+                }
+            }
+
+            return numberOfSubStringsGreaterThanX;
+        }
+    }
+}
---
+++ Tests.Unit/SubStringsGreaterThanXCounterTests.cs
@@ -0,0 +1,25 @@
+﻿using CountNumberOfSubStringsGreaterThanX;
+using NUnit.Framework;
+
+namespace Tests.Unit
+{
+    [TestFixture]
+    public class SubStringsGreaterThanXCounterTests
+    {
+        [TestCase("471", 47, 2)]
+        [TestCase("2222", 97, 3)]
+        [TestCase("123", 100, 1)]
+        [TestCase("1051", 50, 3)]
+        public void Compute_GivenStringAndValue_ComputesNumberOfSubStringGreaterThanValue(string str, int x, int expected)
+        {
+            // Arrange
+            var sut = new SubStringsGreaterThanXCounter();
+
+            // Act
+            var actual = sut.Count(str, x);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}

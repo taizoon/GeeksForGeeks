7abaadd8ec0cf48a246c6b468d6007ddc2bdebe0
Taizoon Miyajiwala <taizoon.tts@gmail.com>
2018-11-12 05:12:12 -0700

    Switched to using ExpectedResult

--- Tests.Unit/SubStringsGreaterThanXCounterTests.cs
+++ Tests.Unit/SubStringsGreaterThanXCounterTests.cs
@@ -6,20 +6,17 @@ namespace Tests.Unit
     [TestFixture]
     public class SubStringsGreaterThanXCounterTests
     {
-        [TestCase("471", 47, 2)]
-        [TestCase("2222", 97, 3)]
-        [TestCase("123", 100, 1)]
-        [TestCase("1051", 50, 3)]
-        public void Compute_GivenStringAndValue_ComputesNumberOfSubStringGreaterThanValue(string str, int x, int expected)
+        [TestCase("471", 47, ExpectedResult = 2)]
+        [TestCase("2222", 97, ExpectedResult = 3)]
+        [TestCase("123", 100, ExpectedResult = 1)]
+        [TestCase("1051", 50, ExpectedResult = 3)]
+        public int Compute_GivenStringAndValue_ComputesNumberOfSubStringGreaterThanValue(string str, int x)
         {
             // Arrange
             var sut = new SubStringsGreaterThanXCounter();
 
             // Act
-            var actual = sut.Count(str, x);
-
-            // Assert
-            Assert.That(actual, Is.EqualTo(expected));
+            return sut.Count(str, x);
         }
     }
 }
